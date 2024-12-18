1844e4940f945b23e6cf3a46b7b7c584ac457d98
Atticus White <contact@atticuswhite.com>
2024-12-18 10:07:59 -0500

    avoid full serialization

--- packages/zustand-ipc-bridge/src/common.ts
+++ packages/zustand-ipc-bridge/src/common.ts
@@ -1,14 +1,13 @@
 export const sanitize = (state: any) => {
-  return JSON.parse(JSON.stringify(state));
-  // TODO: recursive
-  // const safeState: Record<string, unknown> = {};
+  // TODO: recursive function removal
+  const safeState: Record<string, unknown> = {};
 
-  // for (const statePropName in state) {
-  //   const stateProp = state[statePropName];
-  //   if (typeof stateProp !== "function") {
-  //     safeState[statePropName] = stateProp;
-  //   }
-  // }
+  for (const statePropName in state) {
+    const stateProp = state[statePropName];
+    if (typeof stateProp !== "function") {
+      safeState[statePropName] = stateProp;
+    }
+  }
 
-  // return safeState;
+  return safeState;
 };
