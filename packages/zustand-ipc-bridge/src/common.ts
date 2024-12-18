169bcf955e3890ed0562d6407a7e483d94e57e11
Atticus White <contact@atticuswhite.com>
2024-12-17 21:57:54 -0500

    recursive serialization

--- packages/zustand-ipc-bridge/src/common.ts
+++ packages/zustand-ipc-bridge/src/common.ts
@@ -1,12 +1,14 @@
 export const sanitize = (state: any) => {
-  const safeState: Record<string, unknown> = {};
+  return JSON.parse(JSON.stringify(state));
+  // TODO: recursive
+  // const safeState: Record<string, unknown> = {};
 
-  for (const statePropName in state) {
-    const stateProp = state[statePropName];
-    if (typeof stateProp !== "function") {
-      safeState[statePropName] = stateProp;
-    }
-  }
+  // for (const statePropName in state) {
+  //   const stateProp = state[statePropName];
+  //   if (typeof stateProp !== "function") {
+  //     safeState[statePropName] = stateProp;
+  //   }
+  // }
 
-  return safeState;
+  // return safeState;
 };
