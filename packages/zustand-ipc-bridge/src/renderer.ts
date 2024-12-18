749e0274c8b1429dcd449f8d3acd1fa0bfaba66b
Atticus White <contact@atticuswhite.com>
2024-12-17 22:01:07 -0500

    sanitize renderer->main

--- packages/zustand-ipc-bridge/src/renderer.ts
+++ packages/zustand-ipc-bridge/src/renderer.ts
@@ -1,8 +1,9 @@
+import { sanitize } from "./common";
 export const ipcMiddleware = (storeCreator: any, bridge: any) => {
   return (set: any, get: any, api: any) => {
     const ipcSet = (val: any, ...args: any[]) => {
       const value = typeof val === "function" ? val(get()) : val;
-      bridge.setState(value);
+      bridge.setState(sanitize(value));
     };
 
     bridge.subscribe(set);
